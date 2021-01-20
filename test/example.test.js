0352d3d3dacb9a9cd776a423894fcae0879d17de
Casey Warren <caseywar1017@gmail.com>
2021-01-19 21:21:26 -0500

    6 items listed. basic css added

--- test/example.test.js
+++ test/example.test.js
@@ -3,7 +3,7 @@ import { renderAlbum } from '../Product/render.js';
 
 const test = QUnit.test;
 
-test('test should take in album and return a li', (expect) => {
+test('test should tak in album and return a li', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const orange = {
@@ -15,7 +15,7 @@ test('test should take in album and return a li', (expect) => {
         price: 10,
     };
 
-    const expected = `<li class=\"album-items\"><h3 class=\"album-name\">Channel Orange</h3><img class=\"album-image\" src=\"../assets/channel-orange.jpg\"><p class=\"album-description\">The debut studio album by American R&amp;B singer and songwriter Frank Ocean</p><p class=\"category\">Alternative R&amp;B</p><p class=\"album-price\">$10</p><button>Add to cart</button></li>`;
+    const expected = `<li class="album-items"><h3 class="album-name">Channel Orange</h3><img class="album-image" src="../assets/channel-orange.jpg"><p class="album-description">The debut studio album by American R&amp;B singer and songwriter Frank Ocean</p><p class="category">Alternative R&amp;B</p><p class="album-price">$10</p><button>Add to cart</button></li>`;
 
     //Act 
     // Call the function you're testing and set the result to a const
